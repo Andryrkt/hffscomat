332f9f5679c8ae71840b52a16602b8203f9658c8
Andryrkt <andryrkt08@gmail.com>
2026-05-06 16:37:29 +0300

    suppression des dossier et fichier dans le css et js

--- Views/js/magasin/utils/spinnerUtils.js
+++
@@ -1,4 +0,0 @@
-export function toggleSpinner(spinnerService, serviceContainer, show) {
-  spinnerService.style.display = show ? "inline-block" : "none";
-  serviceContainer.style.display = show ? "none" : "block";
-}
