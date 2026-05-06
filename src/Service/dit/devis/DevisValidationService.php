7e1eae8de8bb4ee524448794ddd4dd5c2434ad5d
Andryrkt <andryrkt08@gmail.com>
2026-05-06 17:01:25 +0300

    suppression des dossier et fichier dans le service

--- src/Service/dit/devis/DevisValidationService.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-namespace App\Service\dit\devis;
-
-class DevisValidationService
-{}
--- src/Service/genererPdf/GeneratePdfBlFut.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace App\Service\genererPdf;
-
-use App\Service\genererPdf\GeneratePdf;
-
-class GeneratePdfBlFut extends GeneratePdf {}
