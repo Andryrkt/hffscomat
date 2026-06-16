8abc0cd87f95e7393075346710c82c770d3391a1
Menja <ranofimenjajam@gmail.com>
2026-06-16 14:33:49 +0300

    DTO pour accuseReception et Bc Soumis

---
+++ src/Dto/Atelier/Dit/soumission/accuseReceptionDto.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Dto\Atelier\Dit\soumission;
+
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+class accuseReceptionDto
+{
+    public ?string $nomClient = null;
+    public ?string $emailClient = null;
+    public ?string $numeroDit = null;
+    public ?string $numeroDevis = null;
+    public ?string $numeroBc = null;
+    public ?string $descriptionBc = null;
+    public int $numeroVersion = 1;
+    public ?string $codeSociete = null;
+    public ?string $statutDevis = null;
+    public ?\DateTime $dateCreation = null;
+    public ?\DateTime $dateBc = null;
+    public ?\DateTime $dateDevis = null;
+    public ?UploadedFile $pieceJoint01 = null;
+    public float $montantDevis = 0.0;
+    public ?string $devise = null;
+}
