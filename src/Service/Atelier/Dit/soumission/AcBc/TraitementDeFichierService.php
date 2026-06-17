527dbe8416229a8195f74f8a6ca39f8d9cd8da5d
Menja <ranofimenjajam@gmail.com>
2026-06-17 09:27:36 +0300

    ajout de PDF pour l'accusé de réception + construction de traitement de fichier

--- src/Service/Atelier/Dit/soumission/AcBc/TraitementDeFichierService.php
+++ src/Service/Atelier/Dit/soumission/AcBc/TraitementDeFichierService.php
@@ -2,4 +2,23 @@
 
 namespace App\Service\Atelier\Dit\soumission\AcBc;
 
-class TraitementDeFichierService {}
+use App\Controller\Traits\PdfConversionTrait;
+use App\Service\fichier\FileUploaderService;
+use App\Service\FusionPdf;
+use App\Service\genererPdf\dit\AcBc\GenererPdfAcSoumis;
+
+class TraitementDeFichierService
+{
+    use PdfConversionTrait;
+
+    private FusionPdf $fusionPdf;
+    private GenererPdfAcSoumis $pdfGenerator;
+    private FileUploaderService $fileUploaderService;
+
+    public function __construct()
+    {
+        $this->pdfGenerator        = new GenererPdfAcSoumis();
+        $this->fileUploaderService = new FileUploaderService($_ENV['BASE_PATH_FICHIER']  . '/dit/ac_bc/');
+        $this->fusionPdf           = $this->fileUploaderService->getFusionPdf();
+    }
+}
