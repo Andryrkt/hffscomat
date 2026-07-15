1537d2ba9c3b262f41f0f10d91c188b23246de84
Menja <ranofimenjajam@gmail.com>
2026-07-15 10:02:09 +0300

    factory + formattage date

--- src/Dto/Magasin/Commande/Soumission/CommandeSoumissionDTO.php
+++ src/Dto/Magasin/Commande/Soumission/CommandeSoumissionDTO.php
@@ -4,17 +4,33 @@ namespace App\Dto\Magasin\Commande\Soumission;
 
 class CommandeSoumissionDTO
 {
-    public ?string $numeroCommande = null;
-    public ?string $codeSociete    = null;
-    public ?\DateTime $dateJour    = null;
-    public ?string $typeCde        = null;
-    public ?int $delaiExpedition   = null;
-    public ?string $numFrn         = null;
-    public ?string $nomFrn         = null;
-    public ?string $responsable    = null;
-    public ?string $libelleAgence  = null;
-    public ?string $libelleService = null;
+    public ?string    $numeroCommande  = null;
+    public ?string    $codeSociete     = null;
+    public ?\DateTime $dateJour        = null;
+    public ?string    $typeCde         = null;
+    public ?int       $delaiExpedition = null;
+    public ?string    $numFrn          = null;
+    public ?string    $nomFrn          = null;
+    public ?string    $responsable     = null;
+    public ?string    $libelleAgence   = null;
+    public ?string    $libelleService  = null;
 
     /** @var list<CommandeSoumissionLigneDTO> */
-    public array $lignes           = [];
+    public array      $lignes          = [];
+
+    public function getDateJourFormatted(): string
+    {
+        if (!$this->dateJour) return "";
+
+        $dateFormatter = new \IntlDateFormatter(
+            'fr_FR',
+            \IntlDateFormatter::FULL,
+            \IntlDateFormatter::NONE,
+            null,
+            null,
+            "EEEE dd MMMM yyyy"
+        );
+
+        return $dateFormatter->format($this->dateJour);
+    }
 }
--- src/Factory/magasin/Commande/Soumission/CommandeSoumissionFactory.php
+++ src/Factory/magasin/Commande/Soumission/CommandeSoumissionFactory.php
@@ -2,4 +2,25 @@
 
 namespace App\Factory\magasin\Commande\Soumission;
 
-class CommandeSoumissionFactory {}
+use App\Dto\Magasin\Commande\Soumission\CommandeSoumissionDTO;
+
+class CommandeSoumissionFactory
+{
+    public function hydrate(array $data): CommandeSoumissionDTO
+    {
+        $dto = new CommandeSoumissionDTO;
+
+        $dto->dateJour        = new \DateTime();
+        $dto->numeroCommande  = "";
+        $dto->codeSociete     = "";
+        $dto->typeCde         = "";
+        $dto->delaiExpedition = "";
+        $dto->numFrn          = "";
+        $dto->nomFrn          = "";
+        $dto->responsable     = "";
+        $dto->libelleAgence   = "";
+        $dto->libelleService  = "";
+
+        return $dto;
+    }
+}
