28a194539db4b734db5c1cf25bf2ead14ace12bb
Andryrkt <andryrkt08@gmail.com>
2026-07-30 16:37:18 +0300

    mise en majuscule commande envoyée

--- src/Model/Atelier/Planning/PlanningMaterielModel.php
+++ src/Model/Atelier/Planning/PlanningMaterielModel.php
@@ -163,7 +163,7 @@ class PlanningMaterielModel extends Model
                     WHEN slor_natcm = 'C' 
                         THEN ( select 
                             case
-                                when fcde_cdeext is not null or fcde_cdeext <> '' or fcde_cdeext = '0' then 'Commande envoyée'
+                                when fcde_cdeext is not null or fcde_cdeext <> '' or fcde_cdeext = '0' then 'COMMANDE ENVOYEE'
                                 else null
                             end 
                         from frn_cde where fcde_soc = slor_soc and fcde_succ = slor_succ and fcde_numcde = slor_numcf)
